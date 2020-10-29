d4393f6a6bbbaf0c241d3c39803f8cf3d3a6b073
Douglas James <douglasrcjames@gmail.com>
2020-10-29 13:31:10 -0700

    Fixed SafeAreaView + Navigation + KeyboardAvoidingView issue
    https://stackoverflow.com/questions/64454207/keyboardavoidingview-react-navigation-safe-area-view-not-working

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react';
-import { View, KeyboardAvoidingView, TextInput, Text, Platform, TouchableWithoutFeedback, Keyboard, ActivityIndicator, SafeAreaView, ScrollView, Button, StatusBar   } from 'react-native';
+import React, { Component, useEffect, useState } from 'react';
+import { View, KeyboardAvoidingView, TextInput, Text, Platform, TouchableWithoutFeedback, Keyboard, ActivityIndicator, SafeAreaView, ScrollView, Button, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator, useHeaderHeight } from '@react-navigation/stack';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { useSafeAreaInsets, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 function TicketStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Ticket" component={TicketScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStatusBarHeight: 0, // Header had increased size with SafeArea for some reason (https://github.com/react-navigation/react-navigation/issues/5936)
+        headerStyle: { 
+          backgroundColor: "dodgerblue",
+          elevation: 0, // remove shadow on Android
+          shadowOpacity: 0, // remove shadow on iOS
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "900",
+          fontSize: 26,
+        },
+      }}>
+      <Stack.Screen name="Ticket">
+        {(props) => <TicketScreen {...props} />}
+      </Stack.Screen>
+      <Stack.Screen name="Chat">
+        {(props) => <ChatScreen {...props} />}
+      </Stack.Screen>
     </Stack.Navigator>
   );
 }
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home screen!</Text>
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Text><FontAwesome5 name={"home"} size={20} color={"dodgerblue"} />Home screen</Text>
     </View>
   );
 }
@@ -26,7 +45,7 @@ function HomeScreen() {
 function TicketScreen(props){
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ticket screen!</Text>
+      <Text>Ticket screen</Text>
       <Button title="Go to Chat" onPress={() => props.navigation.navigate('Chat')} />
     </View>
   );
@@ -34,12 +53,28 @@ function TicketScreen(props){
 
 function CustomKeyboardAvoidingView({ children, style }) {
     const headerHeight = useHeaderHeight();
+    console.log("headerHeight: " + headerHeight)
+    console.log("StatusBar.currentHeight: " + StatusBar.currentHeight)
+
+    const insets = useSafeAreaInsets();
+    const [bottomPadding, setBottomPadding] = useState(insets.bottom)
+    const [topPadding, setTopPadding] = useState(insets.top)
+
+    useEffect(() => {
+      // This useEffect is needed because insets are undefined at first for some reason
+      // https://github.com/th3rdwave/react-native-safe-area-context/issues/54
+      setBottomPadding(insets.bottom)
+      setTopPadding(insets.top)
+
+      console.log("topPadding: " + topPadding)
+      console.log("bottomPadding: " + bottomPadding)
+    }, [insets.bottom, insets.top])
 
     return (
         <KeyboardAvoidingView
             style={style}
             behavior={Platform.OS == "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={headerHeight + StatusBar.currentHeight}
+            keyboardVerticalOffset={headerHeight + topPadding}
         >
             {children}
         </KeyboardAvoidingView>
@@ -48,7 +83,6 @@ function CustomKeyboardAvoidingView({ children, style }) {
 
 function ChatScreen(){
   return(
-    <SafeAreaView SafeAreaView style={{flex: 1, backgroundColor: "grey"}}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <CustomKeyboardAvoidingView style={{backgroundColor: "#fff", flex: 1, flexDirection: "column", justifyContent: "space-between" }}>
           <View style={{backgroundColor: "dodgerblue", paddingVertical: 15}}>
@@ -85,25 +119,68 @@ function ChatScreen(){
           </ScrollView>
           
           <View style={{backgroundColor: "yellow", paddingVertical: 15}}>
-              <TextInput placeholder="Type your message here" />
+              <TextInput placeholder="Type your message here..." />
           </View>
         </CustomKeyboardAvoidingView>
       </TouchableWithoutFeedback>
-    </SafeAreaView>
   )
 }
 
 const Tab = createBottomTabNavigator();
 
+function SafeAreaNavigation(){
+  return(
+    <SafeAreaView style={{flex: 1, backgroundColor: "dodgerblue"}}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home';
+              } else if (route.name === 'Ticket') {
+                iconName = 'question';
+              }
+              return <FontAwesome5 name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            style: {
+              height: 70,
+              backgroundColor: "dodgerblue"
+            }, 
+            activeTintColor: "#fff",
+            inactiveTintColor: "dodgerblue",
+            inactiveBackgroundColor: "#fff",
+            activeBackgroundColor: "dodgerblue",                               
+            tabStyle: {
+              paddingTop: 10,
+              paddingBottom: 10,
+              height: 70
+            },
+            labelStyle: {
+              fontSize: 14
+            },
+          }}>
+          <Tab.Screen name="Home">
+            {(props) => <HomeScreen {...props} />}
+          </Tab.Screen>
+          <Tab.Screen name="Ticket">
+            {(props) => <TicketStack {...props} />}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  )
+}
+
 export default class App extends Component {
   render(){
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Ticket" component={TicketStack} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <SafeAreaNavigation />
+      </SafeAreaProvider>
     );
   }
 }
